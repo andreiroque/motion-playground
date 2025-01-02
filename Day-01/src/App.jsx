be2a997a8792706fa972d05e0ed4ba5ef87f9d47
Andrei Roque <roqueandrei0316@gmail.com>
2025-01-02 23:52:25 +0800

    Dynamically render 'card' task

--- Day-01/src/App.jsx
+++ Day-01/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Input } from "./components/ui/input";
 import { Button } from "./components/ui/button";
+import { motion, AnimatePresence } from "framer-motion";
 
 const App = () => {
   const [tasks, setTask] = useState([]);
@@ -17,10 +18,19 @@ const App = () => {
   return (
     <div className="h-screen flex justify-center items-center flex-col gap-5 font-mono">
       <div className="grid grid-cols-3 grid-rows-3 gap-5">
-        <div className="flex justify-center items-center p-5 flex-col gap-10 rounded-md border-2 min-w-40">
-          <p className="text-3xl">Hehe</p>
-          <Button variant="outline">Done</Button>
-        </div>
+        {tasks.map((task) => (
+          <AnimatePresence key={task.id}>
+            <motion.div
+              initial={{ opacity: 0, x: 200 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, y: 50 }}
+              className="flex justify-center items-center p-5 flex-col gap-10 rounded-md border-2 min-w-40"
+            >
+              <p className="text-3xl">{task.name}</p>
+              <Button variant="outline">Done</Button>
+            </motion.div>
+          </AnimatePresence>
+        ))}
       </div>
       <div className="flex gap-5 mt-10">
         <Input ref={taskRef} placeholder="Task" />
