7413276b3e2dc288bac0ed10d08e8e9b6117cd28
Andrei Roque <roqueandrei0316@gmail.com>
2025-01-02 23:44:05 +0800

    Add some basic functionality

--- Day-01/src/App.jsx
+++ Day-01/src/App.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
 
 const App = () => {
+  const [tasks, setTask] = useState([]);
+
+  const taskRef = useRef();
+
+  const addNewTask = () => {
+    if (taskRef.current.value != "") {
+      setTask([...tasks, { id: Date.now(), name: taskRef.current.value }]);
+    }
+    taskRef.current.value = "";
+  };
+
   return (
-    <div>
-      <p>App</p>
+    <div className="h-screen flex justify-center items-center flex-col gap-5 font-mono">
+      <div className="grid grid-cols-3 grid-rows-3 gap-5"></div>
+      <div className="flex gap-5 mt-10">
+        <Input ref={taskRef} placeholder="Task" />
+        <Button onClick={addNewTask}>Add Task</Button>
+      </div>
     </div>
   );
 };
