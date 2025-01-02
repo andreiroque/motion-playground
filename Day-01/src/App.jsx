a861a736714e4c3f5e5e58a6bd420935829444a9
Andrei Roque <roqueandrei0316@gmail.com>
2025-01-02 23:57:48 +0800

    Add 'remove task' functionality and fix some buggy codes

--- Day-01/src/App.jsx
+++ Day-01/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
-import { Input } from "./components/ui/input";
-import { Button } from "./components/ui/button";
 import { motion, AnimatePresence } from "framer-motion";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 const App = () => {
   const [tasks, setTask] = useState([]);
@@ -9,32 +9,42 @@ const App = () => {
   const taskRef = useRef();
 
   const addNewTask = () => {
-    if (taskRef.current.value != "") {
-      setTask([...tasks, { id: Date.now(), name: taskRef.current.value }]);
+    if (taskRef.current.value !== "") {
+      setTask([...tasks, { id: Date.now(), text: taskRef.current.value }]);
     }
     taskRef.current.value = "";
   };
 
+  const removeTask = (id) => {
+    setTask((prevTasks) => prevTasks.filter((task) => task.id !== id));
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center flex-col gap-5 font-mono">
-      <div className="grid grid-cols-3 grid-rows-3 gap-5">
-        {tasks.map((task) => (
-          <AnimatePresence key={task.id}>
+    <div className="h-screen flex items-center justify-center flex-col gap-5 font-mono">
+      <div className="grid gap-5 grid-cols-3 grid-rows-3">
+        <AnimatePresence>
+          {tasks.map((task) => (
             <motion.div
+              key={task.id}
               initial={{ opacity: 0, x: 200 }}
               animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, y: 50 }}
+              exit={{ opacity: 0, y: -50 }}
+              layout
               className="flex justify-center items-center p-5 flex-col gap-10 rounded-md border-2 min-w-40"
             >
-              <p className="text-3xl">{task.name}</p>
-              <Button variant="outline">Done</Button>
+              <p className="text-3xl">{task.text}</p>
+              <Button variant="outline" onClick={() => removeTask(task.id)}>
+                Done
+              </Button>
             </motion.div>
-          </AnimatePresence>
-        ))}
+          ))}
+        </AnimatePresence>
       </div>
-      <div className="flex gap-5 mt-10">
-        <Input ref={taskRef} placeholder="Task" />
-        <Button onClick={addNewTask}>Add Task</Button>
+      <div>
+        <div className="flex gap-5">
+          <Input ref={taskRef} placeholder="Task" />
+          <Button onClick={addNewTask}>Add Task</Button>
+        </div>
       </div>
     </div>
   );
